a99a20ed57f9d7ad7ef2ec6ce871aed83e4a23c8
unknown <emmco96@gmail.com>
2025-08-21 02:30:06 +0100

    new

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+  origin: [process.env.FRONTEND_URL, "https://zippy-theta.vercel.app/"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
