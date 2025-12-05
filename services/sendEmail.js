7dd23f342103e70def2f8f5fbadcee65915340c5
unknown <emmco96@gmail.com>
2025-12-05 16:28:11 +0100

    new

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 async function sendEmail(toEmail, subject, message) {
   try {
-    const response = await fetch("https://connecta.uk/send_email2.php", {
+    const response = await fetch("https://zippypay.ng/send_email.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
